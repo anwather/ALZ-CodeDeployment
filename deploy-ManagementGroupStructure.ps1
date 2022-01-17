16c4ebae624dfb10c5eb521b3e620737cb32cb16
Anthony Watherston <anwather@microsoft.com>
2022-01-17 14:05:35 +1100

    Added workflow (#6)
    * Added workflow
    
    * add install tasks
    
    * Remove mv command
    
    * Add con error
    
    * fix script extension
    
    * verbiose
    
    * Remove depth
    
    * recurse
    
    * Change to windows
    
    * Add call operator
    
    * Fixed slash direction
    
    * yy
    
    * added hello
    
    * Explicit run
    
    * Slash change
    
    * output
    
    * Updated tenant id
    
    * Update workflow tasks
    
    * Remove output
    
    * Add policy objects
    
    * Change slashes
    
    * Revert template changes
    
    * Added management deployment

--- deploy-ManagementGroupStructure.ps1
+++ deploy-ManagementGroupStructure.ps1
@@ -1,20 +1,20 @@
-. .\utilities\functions.ps1
+. ./utilities/functions.ps1
 
-$globals = Get-Content -Path .\globals.json | ConvertFrom-Json
+$globals = Get-Content -Path ./globals.json | ConvertFrom-Json
 
-$v = Get-Content .\templates\mgStructure.json | ConvertFrom-Json
+$v = Get-Content ./templates/mgStructure.json | ConvertFrom-Json
 
 foreach ($tlManagementGroup in $v.($globals.tenantId)) {
-    New-AzDeployment -Location australiaeast -TemplateFile .\bicep\mg.bicep -Id $tlManagementGroup.id -DisplayName $tlManagementGroup.displayName -parent $globals.tenantId -Verbose
+    New-AzDeployment -Location australiaeast -TemplateFile ./bicep/mg.bicep -Id $tlManagementGroup.id -DisplayName $tlManagementGroup.displayName -parent $globals.tenantId -Verbose
     if ($null -ne $tlManagementGroup.children) {
         foreach ($tlChild in $tlManagementGroup.children) {
-            New-AzDeployment -Location australiaeast -TemplateFile .\bicep\mg.bicep -Id $tlChild.id -DisplayName $tlChild.displayName -parent $tlManagementGroup.id -Verbose
+            New-AzDeployment -Location australiaeast -TemplateFile ./bicep/mg.bicep -Id $tlChild.id -DisplayName $tlChild.displayName -parent $tlManagementGroup.id -Verbose
             if ($null -ne $tlChild.children) {
                 foreach ($tlSubChild in $tlChild.children) {
-                    New-AzDeployment -Location australiaeast -TemplateFile .\bicep\mg.bicep -Id $tlSubChild.id -DisplayName $tlSubChild.displayName -parent $tlChild.id -Verbose
+                    New-AzDeployment -Location australiaeast -TemplateFile ./bicep/mg.bicep -Id $tlSubChild.id -DisplayName $tlSubChild.displayName -parent $tlChild.id -Verbose
                     if ($null -ne $tlSubChild.subscriptions) {
                         $tlSubchild.subscriptions | Foreach-Object {
-                            New-AzDeployment -templatefile .\bicep\sub.bicep -Location australiaeast -subscriptionId $_ -parent $tlSubChild.id -Verbose
+                            New-AzDeployment -templatefile ./bicep/sub.bicep -Location australiaeast -subscriptionId $_ -parent $tlSubChild.id -Verbose
                         }
                     }
                 }
