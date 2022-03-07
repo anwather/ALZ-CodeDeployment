f4caee03ff189404bb13b9ba4b0dab01307999eb
Anthony Watherston <anwather@microsoft.com>
2022-03-08 10:04:20 +1100

    Add policy cycle pipelines (#21)

--- utilities/runPolicyRemediation.ps1
+++ utilities/runPolicyRemediation.ps1
@@ -1,4 +1,18 @@
-Get-AzSubscription | Where-Object state -eq "Enabled" | Foreach-Object {
+$subArray = @()
+
+. .\utilities\functions.ps1
+
+$globals = Get-Content -Path .\globals.json | ConvertFrom-Json
+
+Get-Content .\templates\mgStructure.json | ForEach-Object {
+    if ($_ -match "[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}") {
+        if ($_ -notmatch $globals.tenantId) {
+            $subArray += $Matches[0]
+        }
+    }
+}
+
+$subArray | Foreach-Object {
     Select-Azsubscription $_
     $nonCompliantPolicies = Get-AzPolicyState | Where-Object { $_.ComplianceState -eq "NonCompliant" -and $_.PolicyDefinitionAction -eq "deployIfNotExists" }
     foreach ($policy in $nonCompliantPolicies) {
